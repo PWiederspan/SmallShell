a380efdfbcdf9aeaf2623ba37b863b5026a11901
Peyton Wiederspan <wiedersp@os1.engr.oregonstate.edu>
2022-02-01 11:38:13 -0800

    Updated cd command to take both relative and absolute path arguments.

--- smallsh.c
+++ smallsh.c
@@ -27,32 +27,67 @@ void list_contents(){
     printf("\n");
 }
 
-void go_to_home(){
-    chdir(getenv("HOME"));
+void change_directory(char *path){
+    char  gdir[2048];
+    char slash_start[2048] = "/";
+    char path_start;
+    path_start = path[0];
+
+    
+    getcwd(gdir, 2048);
+    if (path_start != "/"){
+        strcat(slash_start, path);
+        strcat(gdir, slash_start);
+
+
+        printf("%s\n", gdir);
+
+        chdir(gdir);
+    }
+
 }
+
 
 int main(){
     char input[2048];
     char command[256];
+    char args[256];
 
     while (strcmp(input, "exit\n") != 0){
+        // Each line in the shell starts with : so that the user knows to enter a command
         printf(": ");
         fgets(input, 2048, stdin);
+
+        // Tokenize the line , delimeted by " ", and save each section into an array with the follow rules
+        // Index 0 = command
+        // Index 1 = arguments
+        // Index 2 = input file
+        // Index 3 = output file
+        // Index 4 = ampersand (to determine background processes)
         int i = 0;
         char *token = strtok(input, " ");
-        char *input_array[6];
-        ///printf("2");
+        char *input_array[6] = {NULL, NULL, NULL, NULL, NULL};
         while (token != NULL){
             input_array[i++] = token;
             token = strtok(NULL, " ");
         }
         strcpy(command, input_array[0]);
         
+        if (strcmp(command, "#\n") == 0){
+            continue;
+        }
+        if (strcmp(command, "\n") == 0){
+            continue;
+        }
         if (strcmp(command, "ls\n") == 0){
             list_contents();
         }
         if (strcmp(command, "cd\n") == 0){
-            go_to_home();
+            chdir(getenv("HOME"));
+        }
+        if (strcmp(command, "cd") == 0){
+            input_array[1][strcspn(input_array[1], "\n")] = 0;
+            change_directory(input_array[1]);
         }
         
     }
